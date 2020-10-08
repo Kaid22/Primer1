80c6a76e0db2d9b4b2df13e2af53c26541a54319
Kaid22 <avdickaid@gmail.com>
2020-10-08 13:47:19 +0200

    commit

--- Primer1/Form1.cs
+++ Primer1/Form1.cs
@@ -16,5 +16,29 @@ namespace Primer1
         {
             InitializeComponent();
         }
+        Random r = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int a = r.Next(60, 100);
+            int b = r.Next(20, 50);
+            int x = r.Next(a / 2, ClientRectangle.Width - a / 2);
+            int y = r.Next(b / 2, ClientRectangle.Width - b / 2);
+            int R = r.Next(256);
+            int G = r.Next(256);
+            int B = r.Next(256);
+            Color bojaOkvira = Color.FromArgb(R, G, B);
+            Color bojaUnutrasnjosti = Color.FromArgb(150, R, G, B);
+            Pen olovka = new Pen(bojaOkvira, 2);
+            SolidBrush cetka = new SolidBrush(bojaUnutrasnjosti);
+            Graphics g = CreateGraphics();
+            g.FillRectangle(cetka, x - a / 2, y - b / 2, a, b);
+            g.DrawRectangle(olovka, x - a / 2, y - b / 2, a, b);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
     }
 }
